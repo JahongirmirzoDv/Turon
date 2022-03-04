31f649f3abc373538658a1a91a1f64d3069ca98a
JahongirmirzoDv <jahongirmirzotolqinov6@gmail.com>
2022-03-04 18:19:25 +0500

    New Additions new Features

--- app/src/main/java/com/example/turon/data/model/Turn.kt
+++ app/src/main/java/com/example/turon/data/model/Turn.kt
@@ -4,9 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class Turn(
     var id:Int,
-    @SerializedName("customer")
     var mijoz:String,
-    @SerializedName("date")
     var sana:String
 
 )
